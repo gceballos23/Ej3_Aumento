d72270aa9ff8862f58cff01dbccf6798f6fce178
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-05-05 23:08:46 -0300

    Ejercicio 4 Ejercicio Aumento de Sueldo

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let btnSend = document.getElementById("send");
+
+btnSend.addEventListener("click", () => {
+  let posicionLLegada: number = Number(data1.value);
+  switch (posicionLLegada) {
+    case 1:
+      console.log("Entregar medalla de oro");
+      break;
+    case 2:
+      console.log("Entregar medalla de plata");
+      break;
+
+    case 3:
+      console.log("Entregar Medalla de Bronce");
+      break;
+    default:
+      console.log("NO recibe medalla");
+  }
+});
