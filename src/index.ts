c6dc9bbb60810a7e2c5e51be14ec4417c6daf5f2
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-05-05 23:25:19 -0300

    Ejercicio 4 Ejercicio 4 aumento de Sueldo

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ btnSend.addEventListener("click", () => {
   let sueldo: number = Number(data.value);
 
   if (sueldo < 15001) {
-    console.log("Aumento 20%", sueldo * 0.2);
+    console.log("Aumento 20 porciento", sueldo * 0.2);
   } else {
     if (sueldo < 20001) {
-      console.log("Aumento 10%", sueldo * 0.1);
+      console.log("Aumento 10 porciento", sueldo * 0.1);
     } else {
       if (sueldo <= 25000) {
-        console.log("Aumento 5%", sueldo * 0.05);
+        console.log("Aumento 5 porciento", sueldo * 0.05);
       } else {
         console.log("No hay Aumento");
       }
